8c71d8259defae3b2c3713010242347428828236
Joseph Birkner <j.birkner@klebert-engineering.de>
2023-05-05 22:59:16 +0200

    Test cases for GeometryCollection with spatial operators.

--- test/ext-geo.cpp
+++ test/ext-geo.cpp
@@ -1,7 +1,10 @@
 #include "simfil/ext-geo.h"
+#include "simfil/model/model.h"
+#include "simfil/simfil.h"
 
 #include <catch2/catch_test_macros.hpp>
 
+using namespace simfil;
 using namespace simfil::geo;
 using BigPoint = Point<double>;
 
@@ -40,4 +43,68 @@ TEST_CASE("Polygon", "[geo.polygon]") {
         REQUIRE(p.contains(BigPoint{0, 0}));         /* top-left */
         REQUIRE(!p.contains(BigPoint{0.5001, 0.5})); /* left to edge */
     };
+}
+
+TEST_CASE("GeometryCollection", "[geom_collection]") {
+
+    SECTION("Construct GeometryCollection") {
+        auto model_pool = std::make_shared<ModelPool>();
+        auto geometry_collection = model_pool->newGeometryCollection();
+
+        REQUIRE(geometry_collection->type() == ValueType::Object);
+        REQUIRE(geometry_collection->size() == 2); // 'type' and 'geometries' fields
+    }
+
+    SECTION("Construct Geometry and Add to GeometryCollection") {
+        auto model_pool = std::make_shared<ModelPool>();
+        auto geometry_collection = model_pool->newGeometryCollection();
+        auto point_geom = geometry_collection->newGeometry(Geometry::GeomType::Points);
+
+        REQUIRE(point_geom->type() == ValueType::Object);
+        REQUIRE(point_geom->geomType() == Geometry::GeomType::Points);
+
+        REQUIRE(geometry_collection->size() == 2); // 'type' and 'geometries' fields
+        REQUIRE(geometry_collection->at(1)->type() == ValueType::Array); // 'geometries' field
+        REQUIRE(geometry_collection->at(1)->size() == 1); // one geometry in the collection
+    }
+}
+
+TEST_CASE("Spatial Operators", "[spatial_ops]") {
+    auto model_pool = std::make_shared<ModelPool>();
+
+    // Create a GeometryCollection with a Point
+    auto geometry_collection = model_pool->newGeometryCollection();
+    auto point_geom = geometry_collection->newGeometry(Geometry::GeomType::Points);
+    point_geom->append({2., 3., 0.});
+    model_pool->addRoot(model_pool->newObject()->addField(
+        "geometry",
+        ModelNode::Ptr(point_geom)));
+    Environment env(model_pool->fieldNames());
+
+    SECTION("Point Within BBox") {
+        auto ast = compile(env, "geo() within bbox(1, 2, 4, 5)", false);
+        INFO("AST: " << ast->toString());
+
+        auto res = eval(env, *ast, *model_pool);
+        REQUIRE(res.size() == 1);
+        REQUIRE(res[0].as<ValueType::Bool>() == true);
+    }
+
+    SECTION("Point Intersects BBox") {
+        auto ast = compile(env, "geo() intersects bbox(1, 2, 4, 5)", false);
+        INFO("AST: " << ast->toString());
+
+        auto res = eval(env, *ast, *model_pool);
+        REQUIRE(res.size() == 1);
+        REQUIRE(res[0].as<ValueType::Bool>() == true);
+    }
+
+    SECTION("BBox Contains Point") {
+        auto ast = compile(env, "bbox(1, 2, 4, 5) contains geo()", false);
+        INFO("AST: " << ast->toString());
+
+        auto res = eval(env, *ast, *model_pool);
+        REQUIRE(res.size() == 1);
+        REQUIRE(res[0].as<ValueType::Bool>() == true);
+    }
 }
