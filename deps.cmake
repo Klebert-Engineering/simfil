e8c6a77c60d250d4472b547f0abd95c27e0a7e11
johannes-wolf <mail@johannes-wolf.com>
2024-02-28 21:33:26 +0100

    cmake: Skip FetchContent if SIMFIL_CONAN is set

--- deps.cmake
+++ deps.cmake
@@ -7,35 +7,31 @@ if (SIMFIL_CONAN)
   if (SIMFIL_WITH_MODEL_JSON)
     find_package(nlohmann_json REQUIRED)
   endif()
-endif()
+else()
+  FetchContent_Declare(sfl
+    GIT_REPOSITORY "https://github.com/slavenf/sfl-library.git"
+    GIT_TAG        "master"
+    GIT_SHALLOW    ON)
 
-FetchContent_Declare(sfl
-  GIT_REPOSITORY "https://github.com/slavenf/sfl-library.git"
-  GIT_TAG        "master"
-  GIT_SHALLOW    ON
-  FIND_PACKAGE_ARGS)
+  FetchContent_Declare(fmt
+    GIT_REPOSITORY "https://github.com/fmtlib/fmt.git"
+    GIT_TAG        "10.2.1"
+    GIT_SHALLOW    ON)
 
-FetchContent_Declare(fmt
-  GIT_REPOSITORY "https://github.com/fmtlib/fmt.git"
-  GIT_TAG        "10.2.1"
-  GIT_SHALLOW    ON
-  FIND_PACKAGE_ARGS)
+  FetchContent_Declare(bitsery
+    GIT_REPOSITORY "https://github.com/fraillt/bitsery.git"
+    GIT_TAG        "v5.2.3"
+    GIT_SHALLOW    ON)
 
-FetchContent_Declare(bitsery
-  GIT_REPOSITORY "https://github.com/fraillt/bitsery.git"
-  GIT_TAG        "v5.2.3"
-  GIT_SHALLOW    ON
-  FIND_PACKAGE_ARGS)
+  FetchContent_MakeAvailable(sfl fmt bitsery)
 
-FetchContent_MakeAvailable(sfl fmt bitsery)
-
-if (SIMFIL_WITH_MODEL_JSON)
-  FetchContent_Declare(nlohmann_json
-    GIT_REPOSITORY "https://github.com/nlohmann/json.git"
-    GIT_TAG        "v3.11.2"
-    GIT_SHALLOW    ON
-    FIND_PACKAGE_ARGS)
-  FetchContent_MakeAvailable(nlohmann_json)
+  if (SIMFIL_WITH_MODEL_JSON)
+    FetchContent_Declare(nlohmann_json
+      GIT_REPOSITORY "https://github.com/nlohmann/json.git"
+      GIT_TAG        "v3.11.2"
+      GIT_SHALLOW    ON)
+    FetchContent_MakeAvailable(nlohmann_json)
+  endif()
 endif()
 
 if (SIMFIL_WITH_TESTS)
