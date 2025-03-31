63fbf5dcf0fc044ba6d3885c492d2d312dc6f35c
Johannes Wolf <mail@johannes-wolf.com>
2025-03-31 09:19:08 +0200

    build: Remove Conan Support

--- deps.cmake
+++ deps.cmake
@@ -1,49 +1,40 @@
 include(FetchContent)
 
-if (SIMFIL_CONAN)
-  find_package(sfl REQUIRED)
-  find_package(fmt REQUIRED)
-  find_package(Bitsery REQUIRED)
-  if (SIMFIL_WITH_MODEL_JSON)
-    find_package(nlohmann_json REQUIRED)
-  endif()
-else()
-  if (SIMFIL_SHARED)
-    set (CMAKE_POSITION_INDEPENDENT_CODE ON)
-  endif ()
+if (SIMFIL_SHARED)
+  set (CMAKE_POSITION_INDEPENDENT_CODE ON)
+endif ()
 
-  if (NOT TARGET sfl::sfl)
-    FetchContent_Declare(sfl
-      GIT_REPOSITORY "https://github.com/slavenf/sfl-library.git"
-      GIT_TAG        "master"
+if (NOT TARGET sfl::sfl)
+  FetchContent_Declare(sfl
+    GIT_REPOSITORY "https://github.com/slavenf/sfl-library.git"
+    GIT_TAG        "master"
+    GIT_SHALLOW    ON)
+  FetchContent_MakeAvailable(sfl)
+endif()
+
+if (NOT TARGET fmt::fmt)
+  FetchContent_Declare(fmt
+    GIT_REPOSITORY "https://github.com/fmtlib/fmt.git"
+    GIT_TAG        "10.0.0"
+    GIT_SHALLOW    ON)
+  FetchContent_MakeAvailable(fmt)
+endif()
+
+if (NOT TARGET bitsery::bitsery)
+  FetchContent_Declare(bitsery
+    GIT_REPOSITORY "https://github.com/fraillt/bitsery.git"
+    GIT_TAG        "v5.2.3"
+    GIT_SHALLOW    ON)
+  FetchContent_MakeAvailable(bitsery)
+endif()
+
+if (SIMFIL_WITH_MODEL_JSON)
+  if (NOT TARGET nlohmann_json::nlohmann_json)
+    FetchContent_Declare(nlohmann_json
+      GIT_REPOSITORY "https://github.com/nlohmann/json.git"
+      GIT_TAG        "v3.11.3"
       GIT_SHALLOW    ON)
-    FetchContent_MakeAvailable(sfl)
-  endif()
-
-  if (NOT TARGET fmt::fmt)
-    FetchContent_Declare(fmt
-      GIT_REPOSITORY "https://github.com/fmtlib/fmt.git"
-      GIT_TAG        "10.0.0"
-      GIT_SHALLOW    ON)
-    FetchContent_MakeAvailable(fmt)
-  endif()
-
-  if (NOT TARGET bitsery::bitsery)
-    FetchContent_Declare(bitsery
-      GIT_REPOSITORY "https://github.com/fraillt/bitsery.git"
-      GIT_TAG        "v5.2.3"
-      GIT_SHALLOW    ON)
-    FetchContent_MakeAvailable(bitsery)
-  endif()
-
-  if (SIMFIL_WITH_MODEL_JSON)
-    if (NOT TARGET nlohmann_json::nlohmann_json)
-      FetchContent_Declare(nlohmann_json
-        GIT_REPOSITORY "https://github.com/nlohmann/json.git"
-        GIT_TAG        "v3.11.3"
-        GIT_SHALLOW    ON)
-      FetchContent_MakeAvailable(nlohmann_json)
-    endif()
+    FetchContent_MakeAvailable(nlohmann_json)
   endif()
 endif()
 
